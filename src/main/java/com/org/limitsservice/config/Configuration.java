c532ced6bc72e4c9e091106975ae7fbacd8a0323
gdmedage <gdmedage@gmail.com>
2020-05-17 00:33:40 +0530

    First Commit

---
+++ src/main/java/com/org/limitsservice/config/Configuration.java
@@ -0,0 +1,32 @@
+package com.org.limitsservice.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+/*
+ *Configuration class read properties file and mapp to minimum and maximum properties.
+ * 
+ */
+@Component
+@ConfigurationProperties("limits-service")
+public class Configuration {
+	
+	private int minimum;
+	private int maximum;
+
+	public void setMinimum(int minimum) {
+		this.minimum = minimum;
+	}
+
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
+	}
+
+	public int getMinimum() {
+		return minimum;
+	}
+
+	public int getMaximum() {
+		return maximum;
+	}
+
+}
